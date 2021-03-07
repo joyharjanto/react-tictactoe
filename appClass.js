2a092ea0d43de9c20a393e49a0efe25567aac3c7
Joy Harjanto <38966343+joyharjanto@users.noreply.github.com>
2021-03-07 15:54:51 -0500

    Update appClass.js

--- appClass.js
+++ appClass.js
@@ -1,36 +1,61 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import NewTask from "../Presentational/NewTask";
 import TasksList from "../Presentational/TasksList";
 
-export default function AppFunction() {
-  const [newTask, setNewTask] = useState({});
-  const handleChange = ({ target }) => {
+export default class AppClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTask: {},
+      allTasks: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleChange({ target }){
     const { name, value } = target;
-    setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
-  };
+    this.setState((prevState) => ({
+      ...prevState,
+      newTask: {
+        ...prevState.newTask,
+        [name]: value,
+        id: Date.now()
+      }
+    }));
+  }
 
-  const [allTasks, setAllTasts] = useState([]);
-  const handleSubmit = (event) => {
+  handleSubmit(event){
     event.preventDefault();
-    if (!newTask.title) return;
-    setAllTasts((prev) => [newTask, ...prev]);
-    setNewTask({});
-  };
-  const handleDelete = (taskIdToRemove) => {
-    setAllTasts((prev) => prev.filter(
-      (task) => task.id !== taskIdToRemove
-    ));
-  };
+    if (!this.state.newTask.title) return;
+    this.setState((prevState) => ({
+      allTasks: [prevState.newTask, ...prevState.allTasks],
+      newTask: {}
+    }));
+  }
 
-  return (
-    <main>
-      <h1>Tasks</h1>
-      <NewTask
-        newTask={newTask}
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-      />
-      <TasksList allTasks={allTasks} handleDelete={handleDelete} />
-    </main>
-  );
+  handleDelete(taskIdToRemove){
+    this.setState((prevState) => ({
+      ...prevState,
+      allTasks: prevState.allTasks.filter((task) => task.id !== taskIdToRemove)
+    }));
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>Tasks</h1>
+        <NewTask
+          newTask={this.state.newTask}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <TasksList
+          allTasks={this.state.allTasks}
+          handleDelete={this.handleDelete}
+        />
+      </main>
+    );
+  }
 }
